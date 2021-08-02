ecf576c2398e6540f6ea445a04ba75d74ca69838
Amantomarekking <67561748+Amantomarekking@users.noreply.github.com>
2021-08-02 11:17:08 +0530

    Update and rename README.md to Phone.py

---
+++ Phone.py
@@ -0,0 +1,13 @@
+# Phone.py
+Phone call details
+import phonenumbers
+from phonenumbers import carrier
+from phonenumbers import geocoder
+from phonenumbers import timezone
+
+a=input("enter number:")
+phone_number=phonenumbers.parse(a)
+print(geocoder.description_for_number(phone_number ,"en"))
+print(carrier.name_for_number(phone_number ,"en"))
+print(timezone.time_zones_for_number(phoneNumber,"en"))
+
